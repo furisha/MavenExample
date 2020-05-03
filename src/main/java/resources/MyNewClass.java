2adac1ffb927a2c9fab3d238e7cb9c148381579b
furisha <vladanfurman@gmail.com>
2020-05-03 17:56:07 +0200

    added class with some comment inside it

---
+++ src/main/java/resources/MyNewClass.java
@@ -0,0 +1,14 @@
+// This is code added newly by me
+
+// This is code added newly by me
+
+// This is code added newly by me
+
+// This is code added newly by me
+
+// This is code added newly by me
+// This is code added newly by me
+// This is code added newly by me
+// This is code added newly by me
+// This is code added newly by me
+
